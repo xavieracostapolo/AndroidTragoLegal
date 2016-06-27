5f9a0bab92b70731747ed14f02121d9242f29381
xavieracostapolo <xavieracosta@ingenieros.com>
2016-06-27 18:58:07 -0500

    Ultima version v1

--- AppTragoLegal/index.android.js
+++ AppTragoLegal/index.android.js
@@ -1,6 +1,10 @@
 'use strict';
 
-import React, { Component } from 'react';
+// 3CM25KWTT1
+
+import  React, { 
+    Component 
+} from 'react';
 // import Icon from 'react-native-vector-icons/Ionicons';
 
 import {
@@ -11,10 +15,14 @@ import {
     TextInput,
     TouchableHighlight,
     ListView,
-    ScrollView
+    ScrollView,
+    Dimensions
 } from 'react-native';
 
+const width = Dimensions.get('window').width;
+
 class AppTragoLegal extends Component {
+
     constructor(props) {
         super(props);
 
@@ -23,24 +31,90 @@ class AppTragoLegal extends Component {
                 rowHasChanged: (row1, row2) => row1 !== row2,
             }),
             codigo : null,
-            tragos : []
+            tragos : [],
+            buscando : false
         }
     }
 
-    /*fetchData() {
-        var urlService = 'https://tragolegal.herokuapp.com/v1/tragolegal/';
+    componentDidMount(){
+        this.refs['txtCodigo'].focus();
+    }
 
-        fetch(urlService + this.state.codigo)
-        .then((response) => response.json())
-        .then((responseData) => {
-            this.setState({
-                dataSource: this.state.dataSource.cloneWithRows(responseData),
-            });
-        })
-        .done();
-    }*/
+    renderLoadingView() {
+        return (
+        <View style={styles.containerBuscando}>
+            <Text>
+                Buscando estampilla ...
+            </Text>
+            <Text>El exceso de alcohol es perjudicial para la salud.</Text>
+            <Text>Prohibido el expendio de bebidas embriagantes a menores de edad.</Text>
+        </View>
+        );
+    }
 
-    tragoSearch(){
+    render() {
+
+        if (this.state.buscando){
+            return this.renderLoadingView();
+        }
+
+        return (
+            <View style={styles.container}>
+                <ScrollView>
+
+                    <View style = {styles.cajaHeader}>
+                        <Text style = {styles.titulo}>
+                            Trago Legal.
+                        </Text>
+                        <Text style={styles.subtitulo}>
+                            Consulta la estampilla de tu botella y verifica que sea trago legal.
+                        </Text>
+                    </View>
+                    
+                    <View style = {styles.formulario}>
+                        
+                        <TextInput 
+                                ref='txtCodigo'
+                                autoCapitalize='characters'
+                                autoCorrect={false}
+                                // autoFocus={true}
+                                maxLength={10}
+                                textAlign='center'
+                                returnKeyType='search'
+                                clearButtonMode='always'
+                                placeholder='Estampilla' 
+                                style={styles.txtCodigo} 
+                                value = {this.state.codigo}
+                                onChange = { event => this.onInputChange(event.nativeEvent.text)}
+                                onSubmitEditing = {()=> this.onPressClearCod()}
+                                blurOnSubmit={false}
+                            />
+
+                    </View>
+                    
+                    <ListView
+                        // enableEmptySections
+                        dataSource={this.state.dataSource}
+                        renderRow={this.renderTrago}
+                        style={styles.listView}
+                    />
+                </ScrollView>
+            </View>
+        );
+    }
+
+    renderTrago(trago) {
+        return (
+        <View style={styles.rowListView}>
+            <Text>Codigo : {trago.codigo}</Text>
+            <Text>Mensaje : {trago.mensaje}</Text>
+            <Text>Fecha : {trago.fechaHora}</Text>
+        </View>
+        );
+    }
+
+    tragoSearch() {
+
         let _component = this;
         
         var trago = {
@@ -54,41 +128,51 @@ class AppTragoLegal extends Component {
         var tmpTrago = _component.state.tragos.slice();
         
         var urlService = 'https://tragolegal.herokuapp.com/v1/tragolegal/';
-        // var urlService = 'http://localhost:8081/v1/tragolegal/';
         
-        fetch(urlService + this.state.codigo)  
-        .then(  
-            function(response) {  
-                if (response.status !== 200) {  
-                    console.log('Looks like there was a problem. Status Code: ' +  
-                    response.status);  
-                    return;  
-                }
+        _component.setState({
+            buscando : true
+        });
 
-                // Examine the text in the response  
-                response.json().then(function(data) {  
-                    
-                    trago.mensaje = data.mensaje;
-                    trago.codigo = data.codigo;
-                    trago.fechaHora = data.fecha;
-                    trago.id = data.id;
-                                      
-                    tmpTrago.push(trago);
-                    
-                    _component.setState({
-                        tragos : tmpTrago
-                    });
-                });  
-            }  
-        )  
+        fetch(urlService + this.state.codigo)  
+        .then(function(response) {
+
+            if (response.status !== 200) {  
+                console.log('Looks like there was a problem. Status Code: ' +  
+                response.status);  
+                return;  
+            }
+
+            // Examine the text in the response  
+            response.json().then(function(data) {  
+                
+                trago.mensaje = data.mensaje;
+                trago.codigo = data.codigo;
+                trago.fechaHora = data.fecha;
+                trago.id = data.id;
+                                    
+                tmpTrago.push(trago);
+
+                var date_sort_desc = function (date1, date2) {
+                    // This is a comparison function that will result in dates being sorted in
+                    // DESCENDING order.
+                    if (date1 > date2) return -1;
+                    if (date1 < date2) return 1;
+                    return 0;
+                };
+
+                tmpTrago.sort(date_sort_desc);
+                
+                _component.setState({
+                    tragos : tmpTrago,
+                    dataSource: _component.state.dataSource.cloneWithRows(tmpTrago),
+                    buscando : false
+                });
+            });  
+        })  
         .catch(function(err) {  
             console.log('Fetch Error :-S', err);  
         });
 
-        this.setState({
-            dataSource: this.state.dataSource.cloneWithRows(
-                this.state.tragos
-            )});
     }
 
     onInputChange(cod) {
@@ -107,119 +191,68 @@ class AppTragoLegal extends Component {
 
         // this.refs['txtCodigo'].focus();
     }
-
-    render() {
-        console.log('Inicio de app');
-        return (
-            <View style={styles.container}>
-
-                <View style = {styles.cajaHeader}>
-                    <Text style = {styles.titulo}>
-                        Trago Legal.
-                    </Text>
-                    <Text style={styles.subtitulo}>
-                        Consulta la estampilla de la botella y verifica que sea trago legal.
-                    </Text>
-                </View>
-                
-                <View style = {styles.formulario}>
-                    
-                    <Text>Ingresa el codigo de la estampilla.</Text>
-                    
-                    <TextInput 
-                            ref='txtCodigo'
-                            autoCapitalize='characters'
-                            autoCorrect={false}
-                            // autoFocus={true}
-                            maxLength={10}
-                            textAlign='center'
-                            returnKeyType='search'
-                            clearButtonMode='always'
-                            placeholder='3CM25KWTT1' 
-                            style={styles.txtCodigo} 
-                            value = {this.state.codigo}
-                            onChange = { event => this.onInputChange(event.nativeEvent.text)}
-                            onSubmitEditing = {()=> this.onPressClearCod()}
-                            blurOnSubmit={false}
-                        />
-                    
-                    <TouchableHighlight onPress={()=> this.onPressClearCod()}>
-                        <Text style = {styles.btnLimpiar}>Limpiar </Text>
-                    </TouchableHighlight>
-                    <Text>
-                        {this.state.codigo}
-                    </Text>
-
-                </View>
-
-                <ScrollView>
-                    <ListView
-                        enableEmptySections
-                        dataSource={this.state.dataSource}
-                        renderRow={this.renderTrago}
-                        style={styles.listView}
-                    />
-                </ScrollView>
-                
-            </View>
-        );
-    }
-
-    renderTrago(trago) {
-        return (
-        <View style={styles.containerListView}>
-            <Text>Estilo : {trago.estilo}</Text>
-            <Text>Codigo : {trago.codigo}</Text>
-            <Text>Mensaje : {trago.mensaje}</Text>
-            <Text>Fecha : {trago.fechaHora}</Text>
-        </View>
-        );
-    }
 }
+
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         paddingTop: 40,
         padding: 10,
-        backgroundColor: '#677676',
+        backgroundColor: '#247BA0',
         justifyContent: 'flex-start',//'center',
 
     },
-    containerListView: {
+    containerBuscando: {
         flex: 1,
         flexDirection: 'column',
-        backgroundColor: '#F5FCFF',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#CDE77F'
+    }, 
+    rowListView: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: '#F0F8EA',
+        borderWidth: 1,
+        borderColor: '#677676',
+        padding: 5
     },
     listView: {
-        paddingTop: 20,
-        backgroundColor: '#F5FCFF',
+        backgroundColor: '#F0F8EA',
     },
     cajaHeader: {
         alignItems: 'center',
         padding: 20,
-        marginBottom: 30,
-        backgroundColor: '#FFFFFF',
+        marginBottom: 20,
+        backgroundColor: '#247BA0',
     },
     formulario: {
+        flexDirection: 'row',
         backgroundColor: '#766554',
-        alignItems: 'center'
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 5
     },
     titulo: {
         fontSize: 30,    
+        color: '#70C1B3'
     },
     subtitulo: {
         fontSize: 10,
-        marginTop: 5
+        marginTop: 5,
+        color: '#70C1B3'
     },
     txtCodigo: {
         height: 70,
         borderWidth: 0.5,
         borderColor: '#0f0f0f',
+        backgroundColor: '#F0F8EA',
+        color: '#13293D',
         flex: 1,
         fontSize: 50,
         padding: 4,
-        width: 300
+        width: width * .8
     },
     btnLimpiar: {
         height: 40,
